a31d8ae68401bd658a995e77cb8a2b80275ca00a
Prasad-Mahajan <105594150+Prasad-Mahajan@users.noreply.github.com>
2024-10-04 11:04:15 +0530

    Add files via upload

---
+++ dashboard/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+
+from mysite.models import transac
+
+def side(request):
+    last=transac.objects.filter().last()
+    line=transac.objects.all()
+    top_five=transac.objects.filter().order_by('-dtime')[:5][::-1]
+    data={'last_data':last,'line':line,'five':top_five}
+    return render(request,'dashboard/sidebar.html',data)
+
+def table(request):
+    # data=transac.objects.all()
+    data=transac.objects.filter().order_by('dtime').reverse()
+    return render(request,'dashboard/table.html',{'data':data})
+
+
